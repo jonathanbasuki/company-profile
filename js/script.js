14ea3dd106f5ec625848552fa21ee29eafe8e1ce
jonathanbasuki <basukijonathan4@gmail.com>
2020-09-07 18:56:20 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// sidebar
+const sideNav = document.querySelectorAll('.sidenav');
+M.Sidenav.init(sideNav);
+
+
+// slider
+const slider = document.querySelectorAll('.slider');
+M.Slider.init(slider, {
+	indicators : false,
+	height : 500,
+	transition : 600,
+	interval : 3000
+});
+
+// parallax
+const parallax = document.querySelectorAll('.parallax');
+M.Parallax.init(parallax);
+
+// portfolio
+const materialbox = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(materialbox);
+
+// scroll spy
+const scroll = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(scroll, {
+	scrollOffset : 50
+});
